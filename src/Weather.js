fc43f2a7eead1a893e1e99e374db99da76494fb2
martinasalayova <vachkovamartina@gmail.com>
2023-08-10 15:21:41 +0200

    Finished basic HTML structure

--- src/Weather.js
+++ src/Weather.js
@@ -10,9 +10,20 @@ export default function Weather() {
       </form>
       <div className="row">
         <div className="col-sm-6">
-          <h1>Prague</h1>
+          <h1>Hradec Králové</h1>
+          <h2>22 °C °F</h2>
+          <ul>
+            <li>Broken clouds</li>
+            <li>Humidity: 42%</li>
+            <li>Wind: 2 Km/h</li>
+          </ul>
         </div>
-        <div className="col-sm-6"></div>
+        <div className="col-sm-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="current weather"
+          ></img>
+        </div>
       </div>
     </div>
   );
