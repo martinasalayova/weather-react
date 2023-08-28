1237ad03ab7b4fdb4d5581240b709d687a1d3007
martinasalayova <vachkovamartina@gmail.com>
2023-08-28 16:26:55 +0200

    Fixed some error messages

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -4,9 +4,9 @@ export default function WeatherForecast(props) {
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col-sm">
+        <div className="col">
           <div className="Day-of-the-week">Mon</div>
-          <WeatherIcon code="01d" size="65" />
+          <WeatherIcon code="01d" size={65} />
           <div className="TwoTemperatures">
             <span className="HighestTemperature">20 /</span>
             <span className="LowestTemperature"> 15</span>
