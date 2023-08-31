c612f3b344d3c2119d260e6e8b6c5efee3d7d5ba
martinasalayova <vachkovamartina@gmail.com>
2023-08-31 20:49:59 +0200

    Added function for showing day of the week in the forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -10,9 +10,19 @@ export default function WeatherForecastDay(props) {
     let temperature = Math.round(props.data.temp.min);
     return `${temperature}°C`;
   }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
   return (
     <div className="WeatherForecastDay">
-      <div className="Day-of-the-week">{props.data.dt}</div>
+      <div className="Day-of-the-week">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={65} />
       <div className="TwoTemperatures">
         <span className="HighestTemperature">{maxTemperature()} /</span>
